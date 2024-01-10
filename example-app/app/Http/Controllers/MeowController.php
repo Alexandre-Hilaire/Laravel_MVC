3f9f77e50412bf5e9c0b75072b9ec040b7724e46
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-01-10 16:45:16 +0100

    Ajout d'une gate pour tester

--- example-app/app/Http/Controllers/MeowController.php
+++ example-app/app/Http/Controllers/MeowController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use Illuminate\Support\Facades\Gate;
 
 class MeowController extends Controller
 {
@@ -13,6 +14,11 @@ class MeowController extends Controller
     }
     public function show_one(string $id){
         $meow = Post::find($id);
+        if (Gate::allows('read-post', $meow)){
         return view('meow-details',['meow' => $meow]);
+        }
+        else {
+            return "vous n'êtes pas autorisé car vous n'êtes pas l'auteur";
+        }
     }
 }
