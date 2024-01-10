bc63c64dab5489eebc6d24d3f0312329130d7af6
Alexandre-Hilaire <alexandre.hilaire@le-campus-numerique.fr>
2024-01-10 11:55:36 +0100

    L'authentification est intallée et fonctionne

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\MeowController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [HomeController::class, "show"]);
-Route::get('/meows', [MeowController::class, "show"]);
-Route::get('/meow/{id}', [MeowController::class, "show_one"]);
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
